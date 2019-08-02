65b8cb6cea741d37e91d6ce144194ea164ebda3f
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2019-08-02 10:50:23 +0200

    Add JWT as authentication method

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
 var genRandomString = function(length){
 	return crypto.randomBytes(Math.ceil(length/2))
 			.toString('hex')
@@ -34,18 +36,15 @@ exports.user_signup = function(req, res) {
 	var email = req.body.email;
     var password = req.body.password;
     if(!username || username.length < 3 || username.length > 15) {
-        res.status(400).send("The username must consist of 3 to 15 characters");
-        res.end();
+        res.status(400).send({message: "The username must consist of 3 to 15 characters."}).end();
     } else if(!validator.validate(email)) {
-        res.status(400).send("Email not valid");
-        res.end();
+        res.status(400).send({message: "Email not valid."}).end();
     } else if(!password || password.length < 5 || password.length > 30) {
-        res.status(400).send("The password must consist of 5 to 30 characters");
-        res.end();
+        res.status(400).send({message: "The password must consist of 5 to 30 characters."}).end();
 	} else {
 		User.exists({ email: email }, function(err, found) { 
 			if(found) {
-				res.status(409).send('Email already used');
+				res.status(409).send({message: "Email already used."}).end();
 				console.log('[user_signup] Email ' + email + ' already in use');
 			} else {
 				var salt = genRandomString(128);
@@ -54,11 +53,9 @@ exports.user_signup = function(req, res) {
 				console.log('[user_signup] New user: ' + new_user);
 				new_user.save(function(err, user) {
 					if (err) {
-						res.send(err);
+						res.send({message: err});
 					}
-					user.password = "";
-					user.salt = "";
-					res.status(201).json(user).end();
+					res.status(201).send({message: 'Sign up completed.'}).end();
 				});
 			}
 		});
@@ -66,11 +63,6 @@ exports.user_signup = function(req, res) {
 }
 
 exports.user_login = function(req, res) {
-    if(req.session.loggedin) {
-        res.status(405).send('Already logged in');
-        res.end();
-        return;
-    }
 	var email = req.params.email;
 	var password = req.body.password;
 	if (email && password) {
@@ -78,35 +70,20 @@ exports.user_login = function(req, res) {
 			if(user) {
 				var hashedPassword = saltPassword(password, user.salt);
 				if(hashedPassword == user.password) {
-					console.log('[user_auth] Auth of: ' + email + ' success!');
-					req.session.loggedin = true;
-					req.session.user = user;
-					res.send('Ok');
+                    console.log('[user_auth] Auth of: ' + email + ' success!');
+                    jwt.sign({user: {_id:user._id, email: user.email}}, 'secretkey', { expiresIn: '2 days' }, (err, token) => {
+                        res.json({ token: token }).end();
+                    });
 				} else {
 					console.log('[user_auth] Auth of: ' + email + ' failed');
-					res.status(401).send('Incorrect Email and/or Password!');
+                    res.status(401).send({message: 'Incorrect Email and/or Password!'}).end();
 				}			
 			} else {
 				console.log('[user_auth] Auth of: ' + email + ' failed');
-				res.status(401).send('Incorrect Email and/or Password!');
+                res.status(401).send({message: 'Incorrect Email and/or Password!'}).end();
             }	
-            res.end();
 		});
 	} else {
-		res.status(401).send('Please enter Email and Password!');
-		res.end();
+		res.status(401).send({message: 'Please enter Email and Password!'}).end();
 	}
-}
-
-exports.user_logout = function(req, res) {
-	if(req.params.email != req.session.user.email) {
-		res.status(401).send('Incorrect Email');
-	} else if(req.session.loggedin) {
-        console.log("[user_logout]" + req.session.user.email + " logout");
-        req.session.loggedin = false;				
-        res.status(200).send("Loggout ok");
-    } else {
-        res.status(405).send("Need to login first");
-    }
-    res.end();
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,5 @@
+var jwt = require('jsonwebtoken');
+
 module.exports = function(app) {
 	var moviesController = require('../controllers/moviesController');
 	var loginController = require('../controllers/loginController');
@@ -10,17 +12,22 @@ module.exports = function(app) {
 	app.route('/signup').get(loginController.show_signup);
 	
 
-	app.route('/api/users')
-		.post(loginController.user_signup)
-	app.route('/api/users/:email')
-		//.put(loginController.update_user)
+	app.route('/api/users').post(loginController.user_signup)
+	app.route('/api/users/:email/token').get(loginController.user_login)
+	//Logout non server più: il token scade da solo dopo 2 giorni
 
-	app.route('/api/users/:email/session')
-		.post(loginController.user_login)
-		.delete(loginController.user_logout)
+	
+	//Esempio: per richiedere la lista delle lobby non server l'authentication code.
+	//		   per aggiungere una lobby invece serve, quindi: "autheticate"
+	app.route('/api/lobby')
+		.get((req, res) => {
+			res.send("Lobbies list");
+		})
+		.post(autheticate, (req, res) => {
+			res.send("Hello " + req.user.email + "!");
+		});
 
 
-		
 	app.route('/api/movies')
 		.get(moviesController.list_movies)
 		.post(moviesController.create_movie);
@@ -31,5 +38,27 @@ module.exports = function(app) {
 		.delete(moviesController.delete_movie);
 
 	app.use(moviesController.show_index);
+
+
+
+	function autheticate(req, res, next) {
+		const bearerHeader = req.headers['authorization'];
+		if(typeof bearerHeader !== 'undefined') {
+			const bearer = bearerHeader.split(' ');
+			const bearerToken = bearer[1];
+			req.token = bearerToken;
+			jwt.verify(req.token, 'secretkey', (err, authData) => {
+				if(err) {
+					res.status(403).send({message: 'Authentication token is not valid.'});
+				} else {
+					req.authData = authData;
+					req.user = authData.user;
+					next();
+				}
+			});
+		} else {
+		  res.status(403).send({message: 'Authentication token undefined.'});
+		}
+	}
 };
 
