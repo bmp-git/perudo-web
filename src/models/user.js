fe72cb97e2b8e61dbe5ef01efa309d85b14d049c
Lorenzo Mondani <lorenzo.mondani@studio.unibo.it>
2019-08-05 19:14:45 +0200

    Add fields to user schema

--- src/models/user.js
+++ src/models/user.js
@@ -18,7 +18,40 @@ var UserSchema = new Schema({
   salt: {
     type: String,
     required: ''
+  },
+  registeredDate: {
+    type: Date,
+    default: Date.now
+  },
+  lastReset: {
+    type: Date,
+    default: Date.now
+  },
+  points: {
+    type: Number,
+    default: 0
+  },
+  wins: {
+    type: Number,
+    default: 0
+  },
+  losses: {
+    type: Number,
+    default: 0
+  },
+  playTime: {
+    type: Number,
+    default: 0
+  },
+  totalPlayTime: {
+    type: Number,
+    default: 0
+  },
+  avatar: {
+    type: String,
+    default: ""
   }
+
 });
 
 module.exports = mongoose.model('User', UserSchema);
