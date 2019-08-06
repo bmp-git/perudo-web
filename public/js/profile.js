e02790099c589a1f5f8597969f0d9e659dfbf9bd
Lorenzo Mondani <lorenzo.mondani@studio.unibo.it>
2019-08-06 12:41:38 +0200

    Fix in profile page.

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,27 +2,21 @@ const Profile = {
     template: `
         <div class="container">
                 
-                <hr />
-                
-                <div class="row">
-                    <div class="col-md-6 offset-md-3">
-                        <h1>Player info</h1>                                          
-                    </div>                                  
-                </div>
-                
+                <hr class="hr-text" data-content="Player info" />
                 
                 <div class="row">
                     <div class="col-md-2 offset-md-3">
-                        <img src="static/img/avatar.png" class="ig-avatar">
+                        <img src="/static/img/avatar.png" class="ig-avatar">
                     </div>
                     <div class="col-md-4">
                         <h2>{{user.username}}</h2>
                         <h4>Italy</h4>
                     </div>
                 </div>
-                
-                <hr />
+                              
 
+                <hr class="hr-text" data-content="Ranking" />
+                
                 
                 <div class="row">
                     <div class="col-md-3 offset-md-3">
@@ -35,12 +29,13 @@ const Profile = {
                     </div>
                 </div>
                 
-                <hr />
-                
+
+                <hr class="hr-text" data-content="Stats" />
+
                 
                 <div class="row">
                     <div class="col-md-6 offset-md-3">
-                        <h2>Stats</h2>
+                        
                         Member since: {{user.registeredDate | formatDate}} <br>
                         Last stat reset: {{user.lastReset | formatDate}} <br>
                         Total play time: {{user.totalPlayTime}} hours <br>
@@ -53,11 +48,10 @@ const Profile = {
                          
                 </div>
                 
-                <hr />
+                <hr class="hr-text" data-content="Play History" />
                 
                 <div class="row">
                     <div class="col-md-6 offset-md-3">
-                        <h2>Play history</h2>
                         Chart <br>                                
                     </div>           
                          
