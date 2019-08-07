f91878c31ee20546b37b3f299f51022fb8a63bba
Lorenzo Mondani <lorenzo.mondani@studio.unibo.it>
2019-08-07 11:19:45 +0200

    Add api for email update.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.change_user_username = function (req, res) {
-    var username = req.body.username;
-    var id = req.params.id;
-    var tokenId = req.authData.user._id;
-    if (tokenId != id) {
+    const username = req.body.username;
+    const id = req.params.id;
+    const tokenId = req.authData.user._id;
+    if (tokenId !== id) {
         res.status(400).send({message: "Id and token aren't compatible."}).end();
     } else if (!username || username.length < 3 || username.length > 15) {
         res.status(400).send({message: "The username must consist of 3 to 15 characters."}).end();
@@ -22,11 +22,33 @@ exports.change_user_username = function (req, res) {
     }
 };
 
+const validator = require("email-validator");
+exports.change_user_email = function (req, res) {
+    const email = req.body.email;
+    const id = req.params.id;
+    const tokenId = req.authData.user._id;
+    if (tokenId !== id) {
+        res.status(400).send({message: "Id and token aren't compatible."}).end();
+    } else if (!validator.validate(email)) {
+        res.status(400).send({message: "Email not valid."}).end();
+    } else {
+        User.findByIdAndUpdate(id, { email: email }, function (err, result) {
+            if (err) {
+                res.status(500).send({message: err}).end();
+            } else if (result) {
+                res.status(200).send({message: "New email set."}).end();
+            } else {
+                res.status(401).send({message: "Incorrect user id."}).end();
+            }
+        });
+    }
+};
+
 exports.change_user_avatar = function (req, res) {
-    var avatar = req.body.avatar;
-    var id = req.params.id;
-    var tokenId = req.authData.user._id;
-    if (tokenId != id) {
+    const avatar = req.body.avatar;
+    const id = req.params.id;
+    const tokenId = req.authData.user._id;
+    if (tokenId !== id) {
         res.status(400).send({message: "Id and token aren't compatible."}).end();
     } else {
         User.findByIdAndUpdate(id, {avatar: avatar}, function (err, result) {
@@ -42,9 +64,9 @@ exports.change_user_avatar = function (req, res) {
 };
 
 exports.get_user_personal_info = function (req, res) {
-    var id = req.params.id;
-    var tokenId = req.authData.user._id;
-    if (tokenId != id) {
+    const id = req.params.id;
+    const tokenId = req.authData.user._id;
+    if (tokenId !== id) {
         res.status(400).send({message: "Id and token aren't compatible."}).end();
     } else {
         User.findById(id, function (err, user) {
@@ -67,7 +89,7 @@ exports.get_user_personal_info = function (req, res) {
 };
 
 exports.get_user_info = function (req, res) {
-    var id = req.params.id;
+    const id = req.params.id;
     User.findById(id, function (err, user) {
         if (err) {
             res.status(500).send({message: err});
@@ -92,15 +114,15 @@ exports.get_user_info = function (req, res) {
 };
 
 exports.get_leaderboard = function (req, res) {
-    var page = req.query.page;
-    var pageLenght = req.query.pageLenght;
+    const page = req.query.page;
+    const pageLenght = req.query.pageLenght;
     if(page && pageLenght && /^[1-9]\d*$/.test(page) && /^[1-9]\d*$/.test(pageLenght)) {
         User.paginate({}, {page : parseInt(page, 10), limit: parseInt(pageLenght, 10), sort : {points : -1}}, function(err, result) {
             if (err) {
                 res.status(500).send({message: err});
             } else {
-                var leaderboard = {result : []};
-                var i = 1;
+                let leaderboard = {result : []};
+                let i = 1;
                 result.docs.forEach(function(user) {
                     leaderboard.result.push({rank: i++, userId: user._id, username: user.username, points : user.points});
                 });
@@ -110,4 +132,4 @@ exports.get_leaderboard = function (req, res) {
     } else {
         res.status(401).send({message: 'Incorrect parameters'}).end();
     }
-}
+};
