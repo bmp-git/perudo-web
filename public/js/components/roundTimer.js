40b6e6746b71b610b73e8f5ddb8d8c6c7b8ba670
Emanuele Pancisi <emanuele.pancisi@studio.unibo.it>
2019-08-14 11:25:34 +0200

    Add customisable round timer refresh time.

--- public/js/components/roundTimer.js
+++ public/js/components/roundTimer.js
@@ -1,28 +1,32 @@
 const roundTimer = { template: `
 <div class="container">
     <div class="progress">
-        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" v-bind:style="'width: '+ (remaining_time * 100) / this.game.turn_time +'%'">{{remaining_time}}</div>
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" v-bind:style="'width: '+ progress_bar +'%'">{{Math.ceil(remaining_time/1000)}}</div>
     </div>
   </div>
 </div>
 `,
-props: ['game'],
+props: ['game','refresh_time_ms'],
  data() {
     return {
         remaining_time: 0,
+        progress_bar : 100,
         current_interval : null
     }
  },
  methods: {
      startTimer: function() {
-        this.remaining_time = this.game.turn_time;
+        this.remaining_time = this.game.turn_time * 1000;
+        this.progress_bar = 100;
         this.current_interval = setInterval(() => {
-            this.remaining_time -= 1;
+            this.remaining_time -= this.refresh_time_ms;
+            this.progress_bar = ((this.remaining_time) * 100 / 1000) / this.game.turn_time;
+
             if(this.remaining_time < 0) {
                 clearInterval(this.current_interval);
                 this.remaining_time = 0;
             }
-        }, 1000)
+        }, this.refresh_time_ms)
      }
  },
  mounted: function () {
