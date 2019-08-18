67569bfec7bc25df0a0203a606abacc937e93eca
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2019-08-18 10:35:27 +0200

    Improve username component efficiency

--- public/js/components/username.js
+++ public/js/components/username.js
@@ -1,10 +1,10 @@
 var usernames = new Map(); //user id -> username
 var waiting_username = new Map(); //user id -> true
 var username_observers = [];
-var cache_username = function(user_id, username) {
+var cache_username = function (user_id, username) {
     usernames.set(user_id, username);
     username_observers.forEach(o => o());
-    console.log("new name for " + user_id + " set");
+    console.log("new name for " + user_id + " set (" + username + ")");
 };
 const Username = {
     template: `<template :key="count">{{username}}</template>`,
@@ -16,14 +16,15 @@ const Username = {
     },
     methods: {
         updateUsername: function () {
-            if (usernames.get(this.userid)) {
+            if (this.$store.state.user && this.userid === this.$store.state.user._id) {
+                this.username = this.$store.state.user.username;
+            } else if (usernames.get(this.userid)) {
                 this.username = usernames.get(this.userid);
-            } else if(!waiting_username.get(this.userid)) {
+            } else if (!waiting_username.get(this.userid)) {
                 waiting_username.set(this.userid, true);
                 axios.get("/api/users/" + this.userid + "/username")
                     .then(response => {
-                        this.username = response.data.username;
-                        cache_username(this.userid, this.username);
+                        cache_username(this.userid, response.data.username);
                         waiting_username.delete(this.userid);
                     })
                     .catch(error => {
@@ -32,12 +33,15 @@ const Username = {
             }
         },
         observer: function () {
-            this.username = usernames.get(this.userid);
+            const username = usernames.get(this.userid);
+            if (username) {
+                this.username = username;
+            }
         }
     },
     mounted: function () {
-        this.updateUsername();
         username_observers.push(this.observer);
+        this.updateUsername();
     },
     destroyed: function () {
         username_observers = username_observers.filter(o => o !== this.observer);
