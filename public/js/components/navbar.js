adf77433e124982272e2ec1f2e3cad9f4432d8ed
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2019-08-20 17:08:44 +0200

    Add icon on navbar

--- public/js/components/navbar.js
+++ public/js/components/navbar.js
@@ -1,7 +1,7 @@
 const Navbar = {
     template : `
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
-	<router-link class="navbar-brand mb-0 h1" data-toggle="collapse" data-target=".navbar-collapse.show" to="/">Perudo.io</router-link>
+	<router-link class="navbar-brand mb-0 h1" data-toggle="collapse" data-target=".navbar-collapse.show" to="/"><img src="/static/img/icons/icon-32x32.png" class="mr-3">Perudo.io</router-link>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	  <span class="navbar-toggler-icon"></span>
 	</button>
