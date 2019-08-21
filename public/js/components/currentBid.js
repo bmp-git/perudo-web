914e439f3706f0c4db1fe2723ddb8b830f9d7b69
Emanuele Pancisi <emanuele.pancisi@studio.unibo.it>
2019-08-21 12:02:08 +0200

    Remove components when game is over in game page.

--- public/js/components/currentBid.js
+++ public/js/components/currentBid.js
@@ -1,7 +1,10 @@
 const currentBid = { template: `
 <div class="container">
     <div class="row d-flex justify-content-center">
-        <template v-if="this.game.current_bid">
+        <template v-if="this.game.is_over">
+            <h4>The game is over</h4>
+        </template>
+        <template v-else-if="this.game.current_bid">
             <h4>Current bid: {{this.game.current_bid.quantity}} dice of <p v-bind:class="'ml-1 dice dice-' + this.game.current_bid.dice"></p>
             </h4>
         </template>
